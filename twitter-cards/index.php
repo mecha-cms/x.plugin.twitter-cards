d206b28929f5c2c34cfd6895870ffff025b0b55d
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-12-17 20:50:25 +0700

    For Mecha 2.2.0

--- twitter-cards/index.php
+++ twitter-cards/index.php
@@ -1,21 +1,21 @@
-<?php
+<?php namespace fn;
 
-function fn_twitter_cards($content) {
-    if ($page = Lot::get('page')) {
-        global $site, $url;
-        $state = Plugin::state(__DIR__);
-        $html  = '<!-- Begin Twitter Cards -->';
-        $html .= '<meta name="twitter:card" content="' . ($page->image ? 'summary_large_image' : 'summary') . '">';
-        $html .= $state['twitter_site'] ? '<meta name="twitter:site" content="@' . $state['twitter_site'] . '">' : "";
-        $html .= $state['twitter_creator'] ? '<meta name="twitter:creator" content="@' . $state['twitter_creator'] . '">' : "";
-        $html .= '<meta name="twitter:title" content="' . To::text($site->page->title) . '">';
-        $html .= '<meta name="twitter:url" content="' . $url->current . '">';
-        $html .= '<meta name="twitter:description" content="' . To::text($page->description($site->description)) . '">';
-        $html .= '<meta name="twitter:image" content="' . $page->image($url . '/favicon.ico') . '">';
-        $html .= '<!-- End Twitter Cards -->';
-        return str_ireplace('</head>', $html . '</head>', $content);
+function twitter_cards($content) {
+    extract(\Lot::get());
+    if (!empty($page)) {
+        $state = \Plugin::state('twitter-cards');
+        $out  = '<!-- Begin Twitter Cards -->';
+        $out .= '<meta name="twitter:card" content="' . ($page->image ? 'summary_large_image' : 'summary') . '">';
+        $out .= $state['twitter_site'] ? '<meta name="twitter:site" content="@' . $state['twitter_site'] . '">' : "";
+        $out .= $state['twitter_creator'] ? '<meta name="twitter:creator" content="@' . $state['twitter_creator'] . '">' : "";
+        $out .= '<meta name="twitter:title" content="' . \To::text($config->trace) . '">';
+        $out .= '<meta name="twitter:url" content="' . $url->current . '">';
+        $out .= '<meta name="twitter:description" content="' . \To::text($page->description ?: $config->description) . '">';
+        $out .= '<meta name="twitter:image" content="' . ($page->image ?: $url . '/favicon.ico') . '">';
+        $out .= '<!-- End Twitter Cards -->';
+        return str_replace('</head>', $out . '</head>', $content);
     }
     return $content;
 }
 
-Hook::set('shield.yield', 'fn_twitter_cards', 1.9);
+\Hook::set('shield.yield', __NAMESPACE__ . "\\twitter_cards", 1.9);
