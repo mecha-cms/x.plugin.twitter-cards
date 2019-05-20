b9ff23b1eeb0f81035967f8baece9b6bdd176420
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-05-20 07:21:59 +0700

    Update

--- twitter-cards/index.php
+++ twitter-cards/index.php
@@ -3,14 +3,14 @@
 function twitter_cards($content) {
     extract($GLOBALS, \EXTR_SKIP);
     if (!empty($page)) {
-        $state = \Plugin::state('twitter-cards');
+        $state = \plugin('twitter-cards');
         $out  = '<!-- Begin Twitter Cards -->';
         $out .= '<meta name="twitter:card" content="' . ($page->image ? 'summary_large_image' : 'summary') . '">';
         $out .= $state['twitter_site'] ? '<meta name="twitter:site" content="@' . $state['twitter_site'] . '">' : "";
         $out .= $state['twitter_creator'] ? '<meta name="twitter:creator" content="@' . $state['twitter_creator'] . '">' : "";
-        $out .= '<meta name="twitter:title" content="' . \To::text($config->trace) . '">';
-        $out .= '<meta name="twitter:url" content="' . \strtr($url->current, ['&' => '&amp;']) . '">';
-        $out .= '<meta name="twitter:description" content="' . \To::text($page->description ?? $config->description) . '">';
+        $out .= '<meta name="twitter:title" content="' . \w($config->trace) . '">';
+        $out .= '<meta name="twitter:url" content="' . \r('&', '&amp;', $url->current) . '">';
+        $out .= '<meta name="twitter:description" content="' . \w($page->description ?? $config->description) . '">';
         $out .= '<meta name="twitter:image" content="' . ($page->image ?? $url . '/favicon.ico') . '">';
         $out .= '<!-- End Twitter Cards -->';
         return \str_replace('</head>', $out . '</head>', $content);
