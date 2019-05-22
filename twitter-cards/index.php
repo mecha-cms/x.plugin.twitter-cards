48d1ff0db1af3d5d3912b0e1a68db18c81050966
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-05-22 10:54:47 +0700

    Update index.php

--- twitter-cards/index.php
+++ twitter-cards/index.php
@@ -8,9 +8,11 @@ function twitter_cards($content) {
         $out .= '<meta name="twitter:card" content="' . ($page->image ? 'summary_large_image' : 'summary') . '">';
         $out .= $state['twitter_site'] ? '<meta name="twitter:site" content="@' . $state['twitter_site'] . '">' : "";
         $out .= $state['twitter_creator'] ? '<meta name="twitter:creator" content="@' . $state['twitter_creator'] . '">' : "";
-        $out .= '<meta name="twitter:title" content="' . \w($config->trace) . '">';
+        $out .= '<meta name="twitter:title" content="' . \w($t->reverse) . '">';
         $out .= '<meta name="twitter:url" content="' . \r('&', '&amp;', $url->current) . '">';
-        $out .= '<meta name="twitter:description" content="' . \w($page->description ?? $config->description) . '">';
+        if ($w = \w($page->description ?? $config->description ?? "")) {
+            $out .= '<meta name="twitter:description" content="' . $w . '">';
+        }
         $out .= '<meta name="twitter:image" content="' . ($page->image ?? $url . '/favicon.ico') . '">';
         $out .= '<!-- End Twitter Cards -->';
         return \str_replace('</head>', $out . '</head>', $content);
