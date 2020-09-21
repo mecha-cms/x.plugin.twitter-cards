4c32098de834f83745dab20c68cb714575d4885c
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-09-22 00:12:56 +0700

    Update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 Twitter Cards Extension for Mecha
 =================================
+
+Release Notes
+-------------
+
+### 2.3.2
+
+ - Make title value clean.
--- twitter-cards/index.php
+++ twitter-cards/index.php
@@ -5,11 +5,11 @@ function twitter_cards($content) {
     if (!empty($page)) {
         $out  = '<!-- Begin Twitter Cards -->';
         $out .= '<meta name="twitter:card" content="' . ($page->image ? 'summary_large_image' : 'summary') . '">';
-        $out .= !empty($state->x->{'twitter-cards'}->site) ? '<meta name="twitter:site" content="@' . $state->x->{'twitter-cards'}->site . '">' : "";
-        $out .= !empty($state->x->{'twitter-cards'}->creator) ? '<meta name="twitter:creator" content="@' . $state->x->{'twitter-cards'}->creator . '">' : "";
-        $out .= '<meta name="twitter:title" content="' . \w($t->reverse) . '">';
+        $out .= !empty($site->x->{'twitter-cards'}->site) ? '<meta name="twitter:site" content="@' . $site->x->{'twitter-cards'}->site . '">' : "";
+        $out .= !empty($site->x->{'twitter-cards'}->creator) ? '<meta name="twitter:creator" content="@' . $site->x->{'twitter-cards'}->creator . '">' : "";
+        $out .= '<meta name="twitter:title" content="' . \w($page->title ?? $t) . '">';
         $out .= '<meta name="twitter:url" content="' . \r('&', '&amp;', $url->current) . '">';
-        if ($w = \w($page->description ?? $state->description ?? "")) {
+        if ($w = \w($page->description ?? $site->description ?? "")) {
             $out .= '<meta name="twitter:description" content="' . $w . '">';
         }
         $out .= '<meta name="twitter:image" content="' . ($page->image ?? $url . '/favicon.ico') . '">';
