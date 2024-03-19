dbf6a8f9332728136c8e21aee9b346fb120d3ed7
Taufik Nurrohman <hi@taufik-nurrohman.com>
2024-03-19 08:24:22 +0700

    Use `lot()` Function

--- index.php
+++ index.php
@@ -4,49 +4,49 @@ function content($content) {
     if (!$content || false === \strpos($content, '</head>')) {
         return $content;
     }
-    \extract($GLOBALS, \EXTR_SKIP);
+    \extract(\lot(), \EXTR_SKIP);
     if (empty($page)) {
         return $content;
     }
-    $out = [false, [], []];
-    $out[1]['twitter:card'] = ['meta', false, [
+    $y = [false, [], []];
+    $y[1]['twitter:card'] = ['meta', false, [
         'content' => $page->image ? 'summary_large_image' : 'summary',
         'name' => 'twitter:card'
     ]];
     if ($creator = $state->x->{'twitter-cards'}->creator ?? "") {
-        $out[1]['twitter:creator'] = ['meta', false, [
+        $y[1]['twitter:creator'] = ['meta', false, [
             'content' => '@' . $creator,
             'name' => 'twitter:creator'
         ]];
     }
     if ($description = \w($page->description ?? $site->description ?? "")) {
-        $out[1]['twitter:description'] = ['meta', false, [
+        $y[1]['twitter:description'] = ['meta', false, [
             'content' => $description,
             'name' => 'twitter:description'
         ]];
     }
-    $out[1]['twitter:image'] = ['meta', false, [
+    $y[1]['twitter:image'] = ['meta', false, [
         'content' => $page->image ?? $url . '/favicon.ico',
         'name' => 'twitter:image'
     ]];
     if ($origin = $state->x->{'twitter-cards'}->site ?? "") {
-        $out[1]['twitter:site'] = ['meta', false, [
+        $y[1]['twitter:site'] = ['meta', false, [
             'content' => '@' . $origin,
             'name' => 'twitter:site'
         ]];
     }
     if ($title = \w($page->title ?? $t ?? "")) {
-        $out[1]['twitter:title'] = ['meta', false, [
+        $y[1]['twitter:title'] = ['meta', false, [
             'content' => $title,
             'name' => 'twitter:title'
         ]];
     }
-    $out[1]['twitter:url'] = ['meta', false, [
+    $y[1]['twitter:url'] = ['meta', false, [
         'content' => $url->current,
         'name' => 'twitter:url'
     ]];
-    $out = new \HTML(\Hook::fire('y.twitter-cards', [$out], $page), true);
-    return \strtr($content, ['</head>' => $out . '</head>']);
+    $v = new \HTML(\Hook::fire('y.twitter-cards', [$y], $page), true);
+    return \strtr($content, ['</head>' => $v . '</head>']);
 }
 
 \Hook::set('content', __NAMESPACE__ . "\\content", 1.9);
